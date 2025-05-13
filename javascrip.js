8aa1dd4a9be17555d5b14bcf98655d3508d99e8d
Adam Hood <Adam.Hood@Adams-MacBook-Pro.local>
2025-05-13 14:30:44 -0500

    multiple emails/pages

---
+++ javascrip.js
@@ -0,0 +1,27 @@
+function josephusSurvivor(n,k){
+    //your code here
+    
+    //given a number n, that is how many number you want to inject into an array
+    let array = []
+    
+    console.log(n);
+
+    for (let i = 1; i <= n; i++) {
+      array.push[i];
+
+    }
+    console.log(array);
+    // for (let i=0; i < (array.length == 1); i++) {
+    //   array.pop[(k - 1)];
+      
+    // }
+    
+    // console.log(array[0]);
+    // return array[0];
+    
+    //Build the array based on the number n
+    //establish the count k
+    //Pop out the value every 3 counts until there is one left
+  }
+
+  josephusSurvivor(4, 2);
